89a2dd2d72c9451b211432a064a0bbdfc1d6da51
MartinHowarth <howarth.martin@gmail.com>
2014-10-15 23:14:05 +0100

    test

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import pygame
 import sys
+from pygame.locals import *
 
 pygame.init()
 
 window = pygame.display.set_mode((640, 480))
 
-pygame.draw.circle(window, (0, 0, 255), (10, 10), 20)
 
-pygame.display.update()
+x, y = 10, 10
+pos = x, y
 
 while 1:
     for event in pygame.event.get():
-        print event
-        if event.type == 12:  # quit
+        # print event
+        if event.type == QUIT:
             pygame.quit()
             sys.exit()
-        elif event.type == 1:
+        elif event.type == MOUSEMOTION:
             pass
-print 1
+        elif event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                x -= 10
+            elif event.key == K_RIGHT:
+                x += 10
+            elif event.key == K_DOWN:
+                y += 10
+            elif event.key == K_UP:
+                y -= 10
+
+            elif event.key == K_ESCAPE:
+                pygame.event.post(pygame.event.Event(QUIT))
+
+        window.fill((255, 255, 255))
+        pygame.draw.circle(window, (0, 0, 255), (x, y), 20)
+
+
+        pygame.display.update()
